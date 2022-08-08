e949bdb0b1351ddbf03176a83bbd3c43aedf9e50
SRin23 <serin4723@naver.com>
2022-08-08 14:57:17 +0100

    init commit

---
+++ src/test/java/com/kimsongnam/mindset/MindsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimsongnam.mindset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindsetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
