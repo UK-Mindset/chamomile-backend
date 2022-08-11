f83077c32f258bb31b0e367ca8ed67d4bd0a2410
SRin23 <serin4723@naver.com>
2022-08-11 04:52:27 +0100

    [add] User Entity 생성 #9

---
+++ src/main/java/com/kimsongnam/mindset/entity/user/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.kimsongnam.mindset.entity.user.repository;
+
+import com.kimsongnam.mindset.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUserEmail(String userEmail);
+    boolean existsByUserNickname(String userNickname);
+    boolean existsByUserPhone(String userPhone);
+
+    Optional<User> findByUserEmail(String userEmail);
+}
