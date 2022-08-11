ac750d4753e65787516a22d29f5a2256f9d234e1
SRin23 <serin4723@naver.com>
2022-08-11 04:52:27 +0100

    [add] User Entity 생성 #9

---
+++ src/main/java/com/kimsongnam/mindset/entity/user/User.java
@@ -0,0 +1,43 @@
+package com.kimsongnam.mindset.entity.user;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="USER_TB")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private long userId;
+
+    @Column(length = 40, nullable = false, name="user_email", unique = true)
+    private String userEmail;
+
+    @Column(length = 60, nullable = false, name="user_password")
+    private String userPassword;
+
+    @Column(length = 20, nullable = false, name = "user_first_name")
+    private String userFirstName;
+
+    @Column(length = 20, nullable = false, name = "user_last_name")
+    private String userLastName;
+
+    @Column(length = 20, nullable = false, name = "user_nickname", unique = true)
+    private String userNickname;
+
+    @Column(length = 20, name="user_phone", unique = true)
+    private String userPhone;
+
+    @Column(name="user_birth")
+    private LocalDateTime userBirth;
+
+    @Column(name="user_gender")
+    private String userGender;
+}
