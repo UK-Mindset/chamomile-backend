94d2d5840bb22bbe488a496beba1ef96cfffbe7c
SRin23 <serin4723@naver.com>
2022-08-12 14:22:46 +0100

    [add] 회원가입 전화번호 형식 검증 #13

--- src/main/java/com/kimsongnam/mindset/dto/request/SignUpUserPhoneRequest.java
+++ src/main/java/com/kimsongnam/mindset/dto/request/SignUpUserPhoneRequest.java
@@ -6,11 +6,13 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class SignUpUserPhoneRequest {
+    @Pattern(regexp = "^(\\+[0-9]{2,}[0-9]{4,}[0-9]*)(x?[0-9]{1,})?$", message = "일치하지 않는 형식입니다.")
     private String userPhone;
 }
