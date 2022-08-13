3e18b12ba609e2046d6485aa24ab3a247a5d089b
SRin23 <serin4723@naver.com>
2022-08-14 00:23:03 +0100

    [add] 감정 enum 구현 #15
    * 다양한 감정에 대한 enum을 구현하였습니다.

---
+++ src/main/java/com/kimsongnam/mindset/entity/mood/MoodCategory.java
@@ -0,0 +1,27 @@
+package com.kimsongnam.mindset.entity.mood;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MoodCategory {
+    BORED,
+    ANXIOUS,
+    IN_LOVE,
+    HURT,
+    MOTIVATE,
+    UPSET,
+    SHOCKED,
+    HUNGRY,
+    SICK,
+    EMBARRASSED,
+    CONFUSE,
+    COOL,
+    QUIET,
+    FUNNY,
+    PROUD,
+    GOOFY,
+    LUCKY,
+    POWERLESS
+}
