946445aaf8e4c5ac4b32d44fa5536231c71fa897
SRin23 <serin4723@naver.com>
2022-08-14 03:26:41 +0100

    [add] 감정 삭제 기능 구현 #17

--- src/main/java/com/kimsongnam/mindset/service/MoodService.java
+++ src/main/java/com/kimsongnam/mindset/service/MoodService.java
@@ -1,6 +1,7 @@
 package com.kimsongnam.mindset.service;
 
 import com.kimsongnam.mindset.dto.request.AddMoodRequest;
+import com.kimsongnam.mindset.dto.request.DeleteMoodRequest;
 import com.kimsongnam.mindset.entity.mood.Mood;
 import com.kimsongnam.mindset.entity.mood.MoodCategory;
 import com.kimsongnam.mindset.entity.mood.MoodSituation;
@@ -8,6 +9,7 @@ import com.kimsongnam.mindset.entity.mood.repository.MoodRepository;
 import com.kimsongnam.mindset.entity.user.User;
 import com.kimsongnam.mindset.entity.user.repository.UserRepository;
 import com.kimsongnam.mindset.exception.BadRequesetException;
+import com.kimsongnam.mindset.exception.ForbiddenException;
 import com.kimsongnam.mindset.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,7 +25,7 @@ public class MoodService {
     @Transactional
     public void AddMood(AddMoodRequest addMoodRequest, BindingResult bindingResult){
         formValidation(bindingResult);
-        User user = userRepository.findById(addMoodRequest.getUserId()).orElseThrow(()-> new NotFoundException("존재하지 않는 회원입니다."));
+        User user = findUser(addMoodRequest.getUserId());
         MoodCategory category = enumCategoryValid(addMoodRequest.getMoodCategory());
         MoodSituation situation = enumSituationValid(addMoodRequest.getMoodSituation());
 
@@ -37,6 +39,15 @@ public class MoodService {
                 .build();
 
         moodRepository.save(mood);
+    }
+
+    @Transactional
+    public void DeleteMood(long moodId, DeleteMoodRequest deleteMoodRequest){
+        Mood mood = findMood(moodId);
+        if(mood.getUserId().getUserId()!= deleteMoodRequest.getUserId()){
+            throw new ForbiddenException("삭제 권한이 없습니다.");
+        }
+        moodRepository.deleteById(moodId);
     }
 
     public void formValidation(BindingResult bindingResult){
@@ -59,5 +70,13 @@ public class MoodService {
         } catch (Exception e) {
             throw new NotFoundException("존재하지 않는 형식의 상황입니다.");
         }
+    }
+
+    public User findUser(long userId){
+        return userRepository.findById(userId).orElseThrow(()-> new NotFoundException("존재하지 않는 회원입니다."));
+    }
+
+    public Mood findMood(long moodId){
+        return moodRepository.findById(moodId).orElseThrow(()-> new NotFoundException("존재하지 않는 감정입니다."));
     }
 }
