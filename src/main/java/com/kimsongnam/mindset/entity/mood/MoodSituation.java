c55a228d6bf7c68254f9dd8786f62dd657dadf8d
SRin23 <serin4723@naver.com>
2022-08-14 01:23:04 +0100

    [add] 감정 상황 enum 구현 #16
    * 해당 감정을 느낀 상황을 선택할 수 있는 enum을 구현하였습니다.

---
+++ src/main/java/com/kimsongnam/mindset/entity/mood/MoodSituation.java
@@ -0,0 +1,22 @@
+package com.kimsongnam.mindset.entity.mood;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MoodSituation {
+    WEATHER,
+    RELATIONS,
+    SLEEPY,
+    WORK,
+    SPORTS,
+    SHOPPING,
+    FRIENDS,
+    HEALTH,
+    FAMILY,
+    TRAVEL,
+    FOOD,
+    EDUCATION
+}
